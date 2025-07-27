24c9673e4e3fba3b587c02b703b197834bd692a7
Shankar Vallabhan <127130512+v3kt0r-87@users.noreply.github.com>
2025-07-28 00:51:06 +0530

    Fixed cloudflare warp i386 warning
    this will make sure amd64 arch is used

--- ubuntu24_04-fix.sh
+++ ubuntu24_04-fix.sh
@@ -108,7 +108,7 @@ clear
 read -rp "Install Cloudflare Warp? (y/n): " install_warp
 if [[ "$install_warp" =~ ^[Yy]$ ]]; then
     curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | \
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | \
         sudo tee /etc/apt/sources.list.d/cloudflare-client.list > /dev/null
     sudo apt update && sudo apt install -y cloudflare-warp
 fi
